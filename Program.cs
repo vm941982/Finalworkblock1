1a00c24b1677be84a35c9ec2106787f538f3006c
Vadim Zavalin <vm941982@outlook.com>
2022-09-19 19:09:49 +0300

    Программа решения

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+    return count;
+}
+string[] AskArray() {
+    Write("enter the value separated by a space: ");
+    return ReadLine().Split(" ");
+}
